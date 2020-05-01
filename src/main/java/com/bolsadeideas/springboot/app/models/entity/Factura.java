968a179ea8e92025e20387a39a7bc688411e13d4
henryserrano <henryserrano@hotmail.com>
2020-05-02 01:28:16 +1000

    Ajustes en descripcion de Factura

--- src/main/java/com/bolsadeideas/springboot/app/models/entity/Factura.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/entity/Factura.java
@@ -19,6 +19,7 @@ import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.NotEmpty;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -33,6 +34,8 @@ public class Factura implements Serializable {
 	@Id
 	@GeneratedValue(strategy =GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotEmpty
 	private String descripcion;
 	private String observacion;
 	
